20227b06856230b81740dcb1e557017c20558e27
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-07-04 10:53:44 +0200

    Check upcoming forecast for rain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 
-API_KEY = 'Example_key'
+#https://openweathermap.org/
+API_KEY = 'YOUR_API_KEY'
 
 # https://www.latlong.net/
 LAT = 48.669102
@@ -10,15 +11,20 @@ parameters = {
     'lat': LAT,
     'lon': LONG,
     'appid': API_KEY,
+    'cnt': 4,
 }
 
 response = requests.get(
-    url='https://api.openweathermap.org/data/2.5/weather',
+    url='https://api.openweathermap.org/data/2.5/forecast',
     params=parameters,
 )
 response.raise_for_status()
-
 data = response.json()
 
-location = data['name']
+weather_data = data['list']
 
+for forecast in weather_data:
+    weather_id = forecast['weather'][0]['id']
+
+    if weather_id < 700:
+        print('Bring an Umbrella')
